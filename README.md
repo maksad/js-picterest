6f48a0aafc64eb8492c7ca879208e33b81443409
maksad <d.maksad@gmail.com>
2017-07-31 19:30:59 +0300

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+## JS Picterest
+
+### User Stories
+* As an unauthenticated user, I can login with Twitter.
+* As an authenticated user, I can link to images.
+* As an authenticated user, I can delete images that I've linked to.
+* As an authenticated user, I can see a Pinterest-style wall of all the images I've linked to.
+* As an unauthenticated user, I can browse other users' walls of images.
+* As an authenticated user, if I upload an image that is broken, it will be replaced by a placeholder image. (can use jQuery broken image detection)
+
+### Heroku app at
+[js-picterest.herokuapp.com](https://js-picterest.herokuapp.com)
---
+++ app/controllers/userController.client.js
@@ -0,0 +1,45 @@
+'use strict';
+
+(function () {
+  var profileId = document.querySelector('#profile-id') || null;
+  var profileUsername = document.querySelector('#profile-username') || null;
+  var profileRepos = document.querySelector('#profile-repos') || null;
+  var displayName = document.querySelector('#display-name');
+  var apiUrl = appUrl + '/api/:id';
+
+  function displayPrivateNavLinks() {
+    var navbarElements = document.querySelectorAll('header nav #nav-mobile li');
+    for (var elem of navbarElements) {
+      elem.classList.remove('hide');
+    }
+    document.querySelector('#login-link').classList.add('hide');
+  }
+
+  function updateHtmlElement (data, element, userProperty) {
+    element.innerHTML = data[userProperty];
+    displayPrivateNavLinks();
+  }
+
+  ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function (data) {
+    var userObject = JSON.parse(data);
+
+    if (userObject.displayName !== null) {
+      updateHtmlElement(userObject, displayName, 'displayName');
+    } else {
+      updateHtmlElement(userObject, displayName, 'username');
+    }
+
+    if (profileId !== null) {
+      updateHtmlElement(userObject, profileId, 'id');
+    }
+
+    if (profileUsername !== null) {
+      updateHtmlElement(userObject, profileUsername, 'username');
+    }
+
+    if (profileRepos !== null) {
+      updateHtmlElement(userObject, profileRepos, 'publicRepos');
+    }
+
+  }));
+})();
